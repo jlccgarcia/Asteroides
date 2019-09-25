c044ef7b3fccefd1994f2a204a3415a258c08eb9
jlccgarcia <55311434+jlccgarcia@users.noreply.github.com>
2019-09-25 19:44:35 +0200

    Tema 3.7 - Añadiendo preferencias de usuario

--- app/src/main/java/com/example/asteroides/MainActivity.java
+++ app/src/main/java/com/example/asteroides/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.asteroides;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,17 +27,36 @@ public class MainActivity extends AppCompatActivity {
     @Override public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            lanzarPreferencias(null);
             return true;
         }
         if (id == R.id.acercaDe) {
             lanzarAcercaDe(null);
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
     public void lanzarAcercaDe(View view){
         Intent i = new Intent(this, AcercaDeActivity.class);
         startActivity(i);
+    }
+
+    public void lanzarPreferencias(View view){
+        Intent i = new Intent(this, PreferenciasActivity.class);
+        startActivity(i);
+    }
+
+    public void mostrarPreferencias(View view){
+        SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String s = "música: "+ pref.getBoolean("musica",true)
+                +", gráficos: " + pref.getString("graficos","?")
+                +", fragmentos: " + pref.getString("fragmentos", "1")
+                +", activar multijugador: " + pref.getBoolean("activar_multijudador", true)
+                +", máximo número jugadores: " +pref.getString("max_jugadores", "1")
+                +", conexión: " + pref.getString("tipo_conexion", "?");
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
